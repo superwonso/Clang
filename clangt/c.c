efd6985c253e3bf212dbf9abe5140df6bca7dea7
superwonso <chadonghyeon@kakao.com>
2020-07-31 21:24:07 +0900

    20200731

--- clangt/c.c
+++ clangt/c.c
@@ -1,41 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-    int H,M;
-    scanf("%d %d",&H,&M);
-    
-    if(H>0)
-    {
-    if (M>45)
-    {
-        printf("%d %d",H,M-45);
-    }
-    else if (M==45)
-    {
-        printf("%d 0",H);
-    }
-    else if (M<45)
-    {
-        printf("%d %d",H-1,15+M);
-    }
-    }
-    
-    else if(H==0)
-{
-      if (M>45)
-    {
-        printf("0 %d",M-45);
-    }
-    else if (M==45)
-    {
-        printf("0 0");
-    }
-    else if (M<45)
-    {
-        printf("23 %d",M+15);
-    }
-}
-return 0;
+    int n,m;
+    scanf("%d %d",&n,&m);
+    printf("%d\n",n/m);
+    printf("%d",n%m);
+
+    return 0;
 }
 // =는 대입, ==는 같음비교. 
-// https://www.acmicpc.net/problem/2884
