0958b2862ceb3504f72c19cc6ff127d0ecf62b2b
Cha DongHyeon <chadonghyeon@kakao.com>
2020-11-22 12:17:19 +0900

    20201122

--- hw/structure_1.c
+++ hw/structure_1.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
-enum days{SUN, MON, TUE, WED, THU, FRI, SAT};
-char* days_name[] = { "sunday", "monday","tuesday","wednesday","thursday","friday","saturday" };
+#define SIZE 3
 
-int main(void)
+struct students{
+   int number;
+   char name[10];
+   double grade;
+};
+
+int main()
 {
-   enum days d;
-   d = WED;
-   printf("%d번째 요일은 %s입니다\n", d, days_name[d]);
-   return 0;
+   struct students list[SIZE];
+   int i;
+   for(i=0; i<SIZE; i++)
+   {
+      printf("학번을 입력하세요 : ");
+      scanf("%d",&list[i].number);
+      printf("이름을 입력하세요 : ");
+      scanf("%s",list[i].name);
+      printf("학점을 입력하세요(실수범위) : ");
+      scanf("%lf",&list[i].grade);
+   }
+   for(i=0; i<SIZE; i++)
+   {
+      printf("학번 : %d, 이름 : %s, 학점: %f\n",list[i].number,list[i].name,list[i].grade);
+   }
+return 0;
 }
