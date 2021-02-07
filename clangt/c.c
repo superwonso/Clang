16e7a0d101334ac52f39daee67268ecb8af794b1
Cha DongHyeon <chadonghyeon@kakao.com>
2021-02-07 15:33:26 +0900

    20210207

--- clangt/c.c
+++ clangt/c.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-    float a,b;
-    float n;
-    scanf("%d %d %d",&a,&b,&n);
-    float c=a/b;
-    for (int i=0; i<n; i++)
-    {
-        a=a*10;
-
-    }
+  float a=100000;
+  int k;
+  while(k=365)
+  {
+      a*=1.02;
+      k+=1;
+  }
     printf("%d",a);
 }
-// =는 대입, ==는 같음비교. 
